e543d629d77910ca9beae10ba528ea75621999a5
James Blair <james.m.blair09@gmail.com>
2019-12-10 17:35:02 +0000

    Dev heading

--- app.R
+++ app.R
@@ -13,7 +13,7 @@ library(shiny)
 ui <- fluidPage(
 
     # Application title
-    titlePanel("Old Faithful Geyser Data"),
+    titlePanel("Old Faithful Geyser Data (Dev)"),
 
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
