578e5d26f369e4dc3e032e4799d42808316ac8d2
James Blair <james.m.blair09@gmail.com>
2021-04-13 19:13:30 +0000

    Change title

--- shiny/app.R
+++ shiny/app.R
@@ -13,7 +13,7 @@ library(shiny)
 ui <- fluidPage(
 
     # Application title
-    titlePanel("New and improved Foo version"),
+    titlePanel("Git Deploy"),
 
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
